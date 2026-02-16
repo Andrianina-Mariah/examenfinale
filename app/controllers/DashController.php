384370a55821f0e6ed7c173e8e2c37266a459018
hallyide <razafindramananaidealy@gmail.com>
2026-02-16 16:30:10 +0300

    page accueil fonctionnelle avec route et controller prenant les donnee depuis la base

--- app/controllers/DashController.php
+++ app/controllers/DashController.php
@@ -5,17 +5,16 @@ class DashController {
       $pdo  = Flight::db();
       $repo = new VilleRepository($pdo);
 
-        Flight::render('/accueil.php', [
-        'villes' => $repo->getAllVilles(),
-        'success' => false
+        Flight::render('front/accueil.php', [
+        'villes' => $repo->getAllVilles()
         ]);
     } catch (Throwable $e) {
       http_response_code(500);
       Flight::json([
         'ok' => false,
         'errors' => [
-          // '_global' => $e->getMessage().' '.$e->getFile().' '.$e->getLine()
-          '_global' => 'Erreur serveur lors de la validation.'
+          '_global' => $e->getMessage().' '.$e->getFile().' '.$e->getLine()
+        //   '_global' => 'Erreur serveur lors de la validation.'
         ],
         'values' => []
       ]);
--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,17 @@
 <?php
 require_once __DIR__ . '/controllers/AuthController.php';
-require_once __DIR__ . '/controllers/MessageController.php';
+require_once __DIR__ . '/controllers/DashController.php';
 
 require_once __DIR__ . '/services/Validator.php';
 require_once __DIR__ . '/services/UserService.php';
 
-// require_once __DIR__ . '/repositories/UserRepository.php';
-require_once __DIR__ . '/repositories/*.php';
+require_once __DIR__ . '/repositories/VilleRepository.php';
+// require_once __DIR__ . '/repositories/*';
 
 /* AUTH */
 Flight::route('GET /', ['DashController', 'DashBoard']);
 
 
-Flight::route('GET /login', ['AuthController', 'login']);
-Flight::route('POST /validate/login', ['AuthController', 'validateLoginAjax']);
+// Flight::route('GET /login', ['AuthController', 'login']);
+// Flight::route('POST /validate/login', ['AuthController', 'validateLoginAjax']);
 
